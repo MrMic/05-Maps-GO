c1afee02790190c458703903b86b2c002ae56936
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-03-26 11:13:03 +0100

    feat: Manipulating Maps

--- main.go
+++ main.go
@@ -3,10 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
-		"red":   "#FF0000",
-		"green": "#00FF00",
-		"blue":  "#0000FF",
-	}
+	// colors := map[string]string
+
+	colors := make(map[string]string)
+
+	// colors := map[string]string{
+	// 	"red":   "#FF0000",
+	// 	"green": "#00FF00",
+	// 	"blue":  "#0000FF",
+	// }
+
+	colors["white"] = "#FFFFFF"
+	delete(colors, "white")
+
 	fmt.Println(colors)
 }
